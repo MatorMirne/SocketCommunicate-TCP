429b698881d8314a5742f947e9138d52e1e0ab90
jaeeun <jaeeun@haegin.kr>
2023-06-26 16:19:23 +0900

    Main문 생성

--- TCPServer/TCPServer/Program.cs
+++ TCPServer/TCPServer/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
 
-Console.WriteLine("Hello, World!");
+    }
+}
