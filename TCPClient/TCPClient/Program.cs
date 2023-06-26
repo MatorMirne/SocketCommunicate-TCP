03417a9158248c3bcc35ee0af6a0df1713d33451
jaeeun <jaeeun@haegin.kr>
2023-06-26 17:16:54 +0900

    서버 클라이언트 TCP 통신 성공

--- TCPClient/TCPClient/Program.cs
+++ TCPClient/TCPClient/Program.cs
@@ -1,7 +1,25 @@
-﻿class Program
+﻿using System.Net;
+using System.Net.Sockets;
+
+class Program
 {
     public static void Main(string[] args)
     {
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        IPEndPoint endPoint = new IPEndPoint(ipAddress, 51225);
 
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        socket.Connect(endPoint);
+        
+        string message = "1";
+        byte[] sendBuffer = System.Text.Encoding.UTF8.GetBytes(message);
+        socket.Send(sendBuffer);
+        
+        byte[] receiveBuffer = new byte[1024];
+        int receiveSize = socket.Receive(receiveBuffer);
+        string receiveMessage = System.Text.Encoding.UTF8.GetString(receiveBuffer, 0, receiveSize);
+        
+        Console.WriteLine(receiveMessage);
+        socket.Close();
     }
 }
--- TCPServer/TCPServer/Program.cs
+++ TCPServer/TCPServer/Program.cs
@@ -1,7 +1,29 @@
-﻿class Program
+﻿using System.Net;
+using System.Net.Sockets;
+
+class Program
 {
     public static void Main(string[] args)
     {
-
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        IPEndPoint endPoint = new IPEndPoint(ipAddress, 51225);
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        
+        serverSocket.Bind(endPoint);
+        serverSocket.Listen(10); // 추후 3만으로 업그레이드
+        Socket clientSocket = serverSocket.Accept(); // 저장된 소켓 정보를 반환
+        
+        byte[] receiveBuffer = new byte[1024];
+        int length = clientSocket.Receive(receiveBuffer, 0, 1024, SocketFlags.None);
+        string receiveMessage = System.Text.Encoding.UTF8.GetString(receiveBuffer, 0, length);
+        Console.WriteLine(receiveMessage);
+        
+        string sendMessage = (int.Parse(receiveMessage) + 1).ToString();
+        byte[] sendBuffer = System.Text.Encoding.UTF8.GetBytes(sendMessage);
+        clientSocket.Send(sendBuffer, 0, sendMessage.Length, SocketFlags.None);
+        Console.WriteLine(sendMessage);
+        
+        clientSocket.Close();
+        serverSocket.Close();
     }
 }
