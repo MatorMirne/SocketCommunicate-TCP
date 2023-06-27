4cd03a08a772a9db43204e656331415b8ae31d62
jaeeun <jaeeun@haegin.kr>
2023-06-27 12:35:25 +0900

    서버 소켓을 안전하게 반환하기 위해 서버를 클래스화 해 보았지만, 프로세스 종료와 동시에 모든 리소스를 반환한다면 불필요할 것 같습니다.

--- TCPServer/TCPServer/Program.cs
+++ TCPServer/TCPServer/Program.cs
@@ -1,50 +1,14 @@
-﻿using System.Net;
-using System.Net.Sockets;
-
-class Program
+﻿namespace Jesus
 {
-    public static void Main(string[] args)
+    class Program
     {
-        Socket serverSocket = SetServer();
-        RunServer(serverSocket);
-        CloseServer(serverSocket);
-    }
-
-    public static Socket SetServer()
-    {
-        IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 51225);
-        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        
-        serverSocket.Bind(endPoint);
-        serverSocket.Listen(10);
-
-        return serverSocket;
-    }
-    
-    public static void RunServer(Socket serverSocket)
-    {
-        while (true)
+        public static void Main(string[] args)
         {
-            Socket clientSocket = serverSocket.Accept();
-
-            byte[] receiveBuffer = new byte[1024];
-            int length = clientSocket.Receive(receiveBuffer, 0, receiveBuffer.Length, SocketFlags.None);
-            string receiveMessage = System.Text.Encoding.UTF8.GetString(receiveBuffer, 0, length);
-            Console.WriteLine(receiveMessage);
-
-            string sendMessage = (int.Parse(receiveMessage) + 1).ToString();
-            byte[] sendBuffer = System.Text.Encoding.UTF8.GetBytes(sendMessage);
-            clientSocket.Send(sendBuffer, 0, sendMessage.Length, SocketFlags.None);
-            Console.WriteLine(sendMessage);
-    
-            clientSocket.Shutdown(SocketShutdown.Both);
-            clientSocket.Close();
+            using (Server server = new())
+            {
+                server.RunServer();
+            }
         }
     }
-    
-    public static void CloseServer(Socket serverSocket)
-    {
-        serverSocket.Shutdown(SocketShutdown.Both);
-        serverSocket.Close();
-    }
 }
+
