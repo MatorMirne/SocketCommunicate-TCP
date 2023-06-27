43131876dfa7662a0a651be996a69dd28191d28c
jaeeun <jaeeun@haegin.kr>
2023-06-27 12:45:10 +0900

    클라이언트 클래스 생성

---
+++ TCPServer/TCPServer/Client.cs
@@ -0,0 +1,38 @@
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPServer
+{
+    public class Client : IDisposable
+    {
+        Socket socket;
+
+        public Client(Socket socket)
+        {
+            this.socket = socket;
+        }
+
+        void Receive()
+        {
+            byte[] receiveBuffer = new byte[1024];
+            int length = socket.Receive(receiveBuffer, 0, receiveBuffer.Length, SocketFlags.None);
+            string receiveMessage = System.Text.Encoding.UTF8.GetString(receiveBuffer, 0, length);
+            Console.WriteLine(receiveMessage);
+        }
+
+        void Send()
+        {
+            string sendMessage = "send message";
+            byte[] sendBuffer = System.Text.Encoding.UTF8.GetBytes(sendMessage);
+            socket.Send(sendBuffer, 0, sendMessage.Length, SocketFlags.None);
+            Console.WriteLine(sendMessage);
+        }
+
+        public void Dispose()
+        {
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+    }
+}
--- TCPServer/TCPServer/Program.cs
+++ TCPServer/TCPServer/Program.cs
@@ -1,14 +1,25 @@
-﻿namespace Jesus
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace TCPServer
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            using (Server server = new())
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 51225);
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            serverSocket.Bind(endPoint);
+            serverSocket.Listen(10); // 추후 3만으로 업그레이드
+
+            while (true)
             {
-                server.RunServer();
+                Socket clientSocket = serverSocket.Accept();
+                Client client = new Client(clientSocket);
             }
+
+            serverSocket.Close();
         }
     }
 }
-
