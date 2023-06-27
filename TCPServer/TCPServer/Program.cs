3325a7f35b991dbe64c74b297715172292077a7a
jaeeun <jaeeun@haegin.kr>
2023-06-27 11:14:21 +0900

    서버 세팅-실행-종료 구분

--- TCPServer/TCPServer/Program.cs
+++ TCPServer/TCPServer/Program.cs
@@ -5,26 +5,46 @@ class Program
 {
     public static void Main(string[] args)
     {
+        Socket serverSocket = SetServer();
+        RunServer(serverSocket);
+        CloseServer(serverSocket);
+    }
+
+    public static Socket SetServer()
+    {
         IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 51225);
         Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         
         serverSocket.Bind(endPoint);
-        serverSocket.Listen(10); // 추후 3만으로 업그레이드
-        Socket clientSocket = serverSocket.Accept();
+        serverSocket.Listen(10);
 
-        byte[] receiveBuffer = new byte[1024];
-        int length = clientSocket.Receive(receiveBuffer, 0, receiveBuffer.Length, SocketFlags.None);
-        string receiveMessage = System.Text.Encoding.UTF8.GetString(receiveBuffer, 0, length);
-        Console.WriteLine(receiveMessage);
-        
-        string sendMessage = (int.Parse(receiveMessage) + 1).ToString();
-        byte[] sendBuffer = System.Text.Encoding.UTF8.GetBytes(sendMessage);
-        clientSocket.Send(sendBuffer, 0, sendMessage.Length, SocketFlags.None);
-        Console.WriteLine(sendMessage);
-        
-        clientSocket.Shutdown(SocketShutdown.Both);
-        clientSocket.Close();
-        
+        return serverSocket;
+    }
+    
+    public static void RunServer(Socket serverSocket)
+    {
+        while (true)
+        {
+            Socket clientSocket = serverSocket.Accept();
+
+            byte[] receiveBuffer = new byte[1024];
+            int length = clientSocket.Receive(receiveBuffer, 0, receiveBuffer.Length, SocketFlags.None);
+            string receiveMessage = System.Text.Encoding.UTF8.GetString(receiveBuffer, 0, length);
+            Console.WriteLine(receiveMessage);
+
+            string sendMessage = (int.Parse(receiveMessage) + 1).ToString();
+            byte[] sendBuffer = System.Text.Encoding.UTF8.GetBytes(sendMessage);
+            clientSocket.Send(sendBuffer, 0, sendMessage.Length, SocketFlags.None);
+            Console.WriteLine(sendMessage);
+    
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+        }
+    }
+    
+    public static void CloseServer(Socket serverSocket)
+    {
+        serverSocket.Shutdown(SocketShutdown.Both);
         serverSocket.Close();
     }
 }
